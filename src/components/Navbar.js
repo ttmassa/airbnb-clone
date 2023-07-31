d869b7697836cb41e478915b218a406824a59188
Massa Timothée <timothee.massa.perso@gmail.com>
2023-07-31 13:31:11 +0200

    Navbar HTML written

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import logoImage from '../images/airbnb-logo.png'
 
 export default function Navbar() {
     return (
         <nav>
-            
+            <img src={logoImage} alt="Airbnb's logo"/>
         </nav>
     );
 }

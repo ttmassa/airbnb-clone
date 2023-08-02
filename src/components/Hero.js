671298fb23701db431d79818be1a7ef53facb00c
Massa Timothée <timothee.massa.perso@gmail.com>
2023-08-02 16:39:52 +0200

    Hero styled

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,12 +3,12 @@ import photoGrid from '../images/photo-grid.png'
 
 export default function Hero() {
     return (
-        <article>
-            <img src={photoGrid} alt='grid of activities'/>
-            <h1>Online Experiences</h1>
-            <h4>Join unique interactive activities led by 
-                one-of-a-kind hosts—all without leaving home.
-            </h4>
+        <article className='hero'>
+                <img className='hero--photo-grid' src={photoGrid} alt='grid of activities'/>
+                <h1 className='hero--header'> Online Experiences</h1>
+                <p className='hero--text'>Join unique interactive activities led by 
+                    one-of-a-kind hosts—all without leaving home.
+                </p> 
         </article> 
     );
 }

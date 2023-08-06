2f18ab006da50f5e3cfd4a7fdf7ac62cad0fb314
Massa Timothée <timothee.massa.perso@gmail.com>
2023-08-06 23:12:05 +0200

    Card styled

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import swimmerImage from '../images/katie-zafares.png'
+import starImage from '../images/star-icon.png'
 
 export default function Card() {
     return (
-        <img />
+        <article className='card--container'>
+            <img className='card--main-image' src={swimmerImage} alt='Swimmer'/>
+            <img className='card--star' src={starImage} alt="star's icon"/>
+            <p className='card--availability'>SOLD OUT</p>
+            <p className='card--rating'>5.0</p>
+            <p className='card--rating-number'>(6)</p>
+            <p className='card--localisation'>. USA</p> 
+            <p className='card--description'>Life lesson with Katie Zaferes</p>
+            <p className='card--price'><strong>From $136</strong> / person</p>
+        </article>
+        
     );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,12 +3,12 @@ import photoGrid from '../images/photo-grid.png'
 
 export default function Hero() {
     return (
-        <article className='hero'>
+        <section className='hero'>
                 <img className='hero--photo-grid' src={photoGrid} alt='grid of activities'/>
                 <h1 className='hero--header'> Online Experiences</h1>
                 <p className='hero--text'>Join unique interactive activities led by 
                     one-of-a-kind hosts—all without leaving home.
                 </p> 
-        </article> 
+        </section> 
     );
 }

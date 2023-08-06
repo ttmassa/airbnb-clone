9fb0134b00a079f2182212696c6810329788d864
Massa Timothée <timothee.massa.perso@gmail.com>
2023-08-06 23:12:30 +0200

    Card and Hero linked together in App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-// import Hero from './components/Hero';
+import Hero from './components/Hero';
 import Card from './components/Card';
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      {/* <Hero /> */}
+      <Hero />
       <Card />
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -6,11 +6,13 @@ export default function Card() {
     return (
         <article className='card--container'>
             <img className='card--main-image' src={swimmerImage} alt='Swimmer'/>
-            <img className='card--star' src={starImage} alt="star's icon"/>
             <p className='card--availability'>SOLD OUT</p>
-            <p className='card--rating'>5.0</p>
-            <p className='card--rating-number'>(6)</p>
-            <p className='card--localisation'>. USA</p> 
+            <div className='card--stats'>
+                <img className='card--star' src={starImage} alt="star's icon"/>
+                <p className='card--rating'>5.0</p>
+                <p className='card--rating-number'>(6)</p>
+                <p className='card--localisation'>• USA</p> 
+            </div>
             <p className='card--description'>Life lesson with Katie Zaferes</p>
             <p className='card--price'><strong>From $136</strong> / person</p>
         </article>

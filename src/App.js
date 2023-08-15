8e6ccdf9a917d7ba11f1398a04970849add8728e
Massa Timothée <timothee.massa.perso@gmail.com>
2023-08-15 19:37:09 +0200

    Card's details written

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero';
 import Card from './components/Card';
+import swimmerImage from './images/katie-zafares.png'
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <Card 
+        img={swimmerImage} 
+        rating="5.0"
+        reviewCount="6"
+        country="France"
+        title="Life Lessons with Katie Zafares"
+        price="136"
+      />
     </div>
   );
 }
